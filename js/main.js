c7675c1910cb44a6088e5c1445dc423ce8b23020
phongStudentDEV <116378201+phongStudentDEV@users.noreply.github.com>
2023-12-28 02:13:48 +0700

    update

--- js/main.js
+++ js/main.js
@@ -14,3 +14,22 @@ overlay.addEventListener("click", () => {
   menuButton.classList.toggle("open");
   overlay.classList.toggle("show");
 });
+document.addEventListener("DOMContentLoaded", function () {
+  var scrollTopBtn = document.getElementById("scrollTopBtn");
+
+  window.addEventListener("scroll", function () {
+    if (
+      document.body.scrollTop > 20 ||
+      document.documentElement.scrollTop > 20
+    ) {
+      scrollTopBtn.style.display = "block";
+    } else {
+      scrollTopBtn.style.display = "none";
+    }
+  });
+
+  scrollTopBtn.addEventListener("click", function () {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  });
+});
